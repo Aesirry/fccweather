f1961b47e78edaa93ab2410ecab547d77b8abce7
Aesirry <jpbeltrami@gmail.com>
2016-11-24 12:47:50 +0800

    Fix small temperature bug

--- scripts.js
+++ scripts.js
@@ -1,62 +1,56 @@
 $(document).ready(function(){
-  var appID = "&APPID=3139e97f00ada161d76c25bf1a53ce3d";
-  var call;
   var temp;
   var fahr;
   var cel;
-  var icon;
-  var iconSource;
-  var descr;
-  var city;
-  var weather;
+
 
   if(navigator.geolocation) {
   // creates the call for the API
     navigator.geolocation.getCurrentPosition(function(position){
       var lat = position.coords.latitude;
       var lon = position.coords.longitude;
-      call = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + appID;
+      var call = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=3139e97f00ada161d76c25bf1a53ce3d";
 
-  // gets weather information
-    $.getJSON(call, function(data){
-      city = data.name;
-      weather = data.weather[0].main;
-      temp = data.main.temp;
-      fahr = (temp * (9/5)) - 459.67;
-      cel = temp - 273.15;
-      icon = data.weather[0].icon;
-      descr = data.weather[0].description;
-      country = data.sys.country;
+      // gets weather information
+      $.getJSON(call, function(data){
+        var city = data.name;
+        var weather = data.weather[0].main;
+        temp = data.main.temp;
+        fahr = (temp * (9/5)) - 459.67;
+        cel = temp - 273.15;
+        var icon = data.weather[0].icon;
+        var descr = data.weather[0].description;
+        var country = data.sys.country;
 
-      // change the weather icon
-      iconSource = "http://openweathermap.org/img/w/" + icon + ".png";
-      $("#weather-icon").attr({
-        src: iconSource,
-        alt: descr
+        // change the weather icon
+        var iconSource = "http://openweathermap.org/img/w/" + icon + ".png";
+        $("#weather-icon").attr({
+          src: iconSource,
+          alt: descr
+        });
+
+        // change the location
+        $("#location").html(city);
+        $("#country").html(country);
+
+        temp = cel;
+        $("#temp-num").html(Math.floor(temp));
+        $("#temp-scale").html("C&deg;");
       });
-
-      // change the location
-      $("#location").html(city);
-      $("#country").html(country);
     });
-  });
 
-  // Changes the temperature scale
-  temp = cel;
-  $("#temp-num").html(Math.floor(temp));
-  $("#temp-scale").html("C&deg;");
-
-  $("#temp-switch").on("click", function(){
-    if(temp == fahr){
-      temp = cel;
-      $("#temp-num").html(Math.floor(temp));
-      $("#temp-scale").html("C&deg;");
-    }
-    else{
-      temp = fahr;
-      $("#temp-num").html(Math.floor(temp));
-      $("#temp-scale").html("F&deg;");
-    }
-  });
-};
+    // Changes the temperature scale
+    $("#temp-switch").on("click", function(){
+      if(temp == fahr){
+        temp = cel;
+        $("#temp-num").html(Math.floor(temp));
+        $("#temp-scale").html("C&deg;");
+      }
+      else{
+        temp = fahr;
+        $("#temp-num").html(Math.floor(temp));
+        $("#temp-scale").html("F&deg;");
+      }
+    });
+  };
 });
